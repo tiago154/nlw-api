865c84337f44f790e9b1b7b6490788fdddb55527
Tiago Silva <tiago.toya@hotmail.com>
2020-06-07 20:53:55 -0300

    fix: ajuste na montagem da url de imagem

--- src/util/image-url-builder.ts
+++ src/util/image-url-builder.ts
@@ -1,3 +1,3 @@
-const port = process.env.PORT ? `:${process.env.PORT}` : ''
+const port = process.env.PORT && process.env.NODE_ENV !== 'production' ? `:${process.env.PORT}` : ''
 
 export default (protocol: string, host: string, path: string, file: string) => `${protocol}://${host}${port}/${path}/${file}`
